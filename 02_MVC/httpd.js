a0ffb75a5cc2d205d04cebd395091060168b4591
Oleksandr Nykytin <nikitin_a@itstep.academy>
2021-09-13 17:45:10 +0300

    express

--- 02_MVC/httpd.js
+++ 02_MVC/httpd.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-const controller = require("./controllers/simpleController");
+// Установить каталог для статических файлов
+app.use(express.static("public"));
 
+const controller = require("./controllers/simpleController");
 app.route ("/1")
     .get(controller.get)
     .post(controller.post);
