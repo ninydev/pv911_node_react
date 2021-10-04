f06522e44ca6dd5d80c4b9efae66d8cbf0654989
Oleksandr Nykytin <nikitin_a@itstep.academy>
2021-10-04 16:18:16 +0300

    React + WebPack

---
+++ 07_reactwebpack/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NovaPoshta from "./componets/NovaPoshta";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <NovaPoshta></NovaPoshta>
+    </div>
+  );
+}
+
+export default App;
---
+++ 07_reactwebpack/src/componets/NovaPoshta.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class NovaPoshta extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Новая почта тут</h1>
+            </div>
+        )
+    }
+}
